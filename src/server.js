12f9fcb7c43885669d79022d8042e836c012fb6b
Hona <nguyenmanhhoan2k4@gmail.com>
2025-07-29 21:57:26 +0700

    generate model/migration/seeder

--- src/server.js
+++ src/server.js
@@ -3,14 +3,19 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from 'body-parser';
+import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+configViewEngine(app);
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-configViewEngine(app);
+//test connection db
+connection();
 
 initWebRoutes(app);
 
