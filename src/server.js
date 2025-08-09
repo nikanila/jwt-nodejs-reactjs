a3077c72fa3ea93ea0b0fd6ae68034b50c243889
Hona <nguyenmanhhoan2k4@gmail.com>
2025-08-09 22:30:53 +0700

    create cors file in config folder and create an  api to create new user

--- src/server.js
+++ src/server.js
@@ -1,35 +1,19 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
+import configCors from "./config/cors";
 require("dotenv").config();
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+//config cors
+configCors(app);
 
 configViewEngine(app);
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,7 +22,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connection();
 
 initWebRoutes(app);
+initApiRoutes(app);
 
 app.listen(PORT, () => {
-    console.log(`JWT Backend is running on the port ${PORT}`);
-})
+  console.log(`JWT Backend is running on the port ${PORT}`);
+});
