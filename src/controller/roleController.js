92267ead3e8a68584c7e053c50df7d0b506f2d3c
Hona <nguyenmanhhoan2k4@gmail.com>
2025-09-29 20:11:13 +0700

    create assign-to-group api and add foreign key for role and group modal

--- src/controller/roleController.js
+++ src/controller/roleController.js
@@ -93,10 +93,29 @@ const getRoleByGroup = async (req, res) => {
   }
 }
 
+const assignRoleToGroup = async (req, res) => {
+  try {
+    let data = await roleApiService.assignRoleToGroup(req.body.data);
+    return res.status(200).json({
+      EM: data.EM, // error message
+      EC: data.EC, //error code
+      DT: data.DT, //data
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      EM: "error from server", // error message
+      EC: "-1", //error code
+      DT: "", //data
+    });
+  }
+}
+
 module.exports = {
   readFunc,
   createFunc,
   updateFunc,
   deleteFunc,
-  getRoleByGroup
+  getRoleByGroup,
+  assignRoleToGroup
 };
